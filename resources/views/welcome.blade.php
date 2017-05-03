9e5bfac3628f36cab8637786272518bf66bbf78c
hemisu <hemisu@qq.com>
2017-05-03 15:27:44 +0800

    edit welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel stock for fycy
                 </div>
 
                 <div class="links">
