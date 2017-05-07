da40e4be2b028d00befecd41b9e144ca9538a073
hemisu <hemisu@qq.com>
2017-05-07 09:34:10 +0800

    添加库存模块

---
+++ app/Admin/Controllers/StockController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Model\Stock;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class StockController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('库存管理');
+            $content->description('车辆库存');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('库存管理');
+            $content->description('车辆库存编辑');
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('库存管理');
+            $content->description('车辆库存添加');
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(Stock::class, function (Grid $grid) {
+
+            // $grid->id('ID')->sortable();
+            $grid->column('name','名称')->sortable();
+            $grid->column('type','型号');
+            $grid->inventory('库存')->display(function ($inventory) {
+                return "<span class='label label-primary'>$inventory</span>";
+            });
+            $grid->price('价格')->display(function ($price) {
+                return "<span class='label label-info'>$price</span>";
+            });
+
+            // $grid->created_at();
+            $grid->updated_at('更新时间');
+            $grid->filter(function($filter){
+                // 禁用id查询框
+                $filter->disableIdFilter();
+                $filter->where(function ($query) {
+                    $query->where('name', 'like', "%{$this->input}%")
+                        ->orWhere('type', 'like', "%{$this->input}%");
+                }, '名称或型号');
+                $filter->between('updated_at', '更新时间')->datetime();
+            });
+
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(Stock::class, function (Form $form) {
+
+            $form->display('id', 'ID');
+            $form->text('name', '名称')->rules('required|min:2');
+            $form->text('type', '型号');
+            $form->number('inventory', '库存');
+            $form->currency('price', '价格')->symbol('￥');
+            $form->display('created_at', 'Created At');
+            $form->display('updated_at', 'Updated At');
+        });
+    }
+}
---
+++ database/migrations/2017_05_03_092516_create_stock_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('type');
+            $table->string('image')->nullable();
+            $table->integer('number');
+            $table->double('price', 15, 2);
+            $table->double('cost', 15, 2)->nullable();
+            $table->integer('inventory');
+            $table->integer('staff_id')->nullable();//经手人
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
