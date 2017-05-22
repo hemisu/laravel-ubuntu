0e34883b8c224fe68d47208dc19a7258ff567a49
hemisu <hemisu@qq.com>
2017-05-22 20:26:25 +0800

    api不同材质显示

--- app/Admin/Controllers/SalesRecordController.php
+++ app/Admin/Controllers/SalesRecordController.php
@@ -120,7 +120,7 @@ class SalesRecordController extends Controller
             $form->hidden('id');
             $form->select('stock_id','车辆型号规格')->options(
               Stock::all()->mapWithKeys(function ($item) {
-                return [$item['id'] => $item['name'].' 规格:'.$item['type'].' 目前零售价:'.$item['price']];
+                return [$item['id'] => $item['name'].' 规格:'.$item['type'].' 材质:'.$item['material'].' 目前零售价:'.$item['price']];
             })
               )->ajax('/admin/api/stock');
             $form->currency('price', '销售金额')->symbol('￥');
