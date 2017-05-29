b7f0babf202845c8d6add06291220170df41f527
hemisu <hemisu@qq.com>
2017-05-29 17:50:55 +0800

    wip

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -8,17 +8,17 @@ class DeploymentController extends Controller
 {
   public function deploy(Request $request)
   {
-    $commands = ['cd /var/www/laravel-ubuntu', 'git pull'];
-    $signature = $request->header('X-Hub-Signature');
-    $payload = file_get_contents('php://input');
-    if ($this->isFromGithub($payload, $signature)) {
-        foreach ($commands as $command) {
-            shell_exec($command);
-        }
+    // $commands = ['cd /var/www/laravel-ubuntu', 'git pull'];
+    // $signature = $request->header('X-Hub-Signature');
+    // $payload = file_get_contents('php://input');
+    // if ($this->isFromGithub($payload, $signature)) {
+    //     foreach ($commands as $command) {
+    //         shell_exec($command);
+    //     }
         http_response_code(200);
-    } else {
-        abort(403);
-    }
+    // } else {
+    //     abort(403);
+    // }
   }
   private function isFromGithub($payload, $signature)
   {
