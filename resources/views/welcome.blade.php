a8addaaa1d655bdbfd892a68e851a0c83421554d
hemisu <hemisu@qq.com>
2017-05-29 17:06:21 +0800

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel stock for fycy
+                    Laravel stock for fycy test
                 </div>
 
                 <div class="links">
