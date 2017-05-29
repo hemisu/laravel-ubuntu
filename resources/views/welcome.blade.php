3aa1c280a31ec827ab8a6294f9595c85bd30fcd9
hemisu <hekunyu@me.com>
2017-05-29 18:14:04 +0800

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel stock for fycy
+                    Laravel stock for fycy 1
                 </div>
 
                 <div class="links">
