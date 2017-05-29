f3195395bbca3406e512c37766a0a171126894fb
hemisu <hekunyu@me.com>
2017-05-29 18:31:51 +0800

    wip

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/deploy', 'DeploymentController@deploy');
+// Route::post('/deploy', 'DeploymentController@deploy');
+Route::get('/info', function(){
+  return phpinfo();
+});
