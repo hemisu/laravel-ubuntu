19386d799ba158fa8d27e63ee7be44d199d3556e
hemisu <hekunyu@me.com>
2017-07-08 01:31:22 +0800

    stockcontroller.php

--- app/Admin/Controllers/SalesRecordController.php
+++ app/Admin/Controllers/SalesRecordController.php
@@ -144,7 +144,7 @@ class SalesRecordController extends Controller
             $form->select('client_id','客户')->options(
                 Client::all()->mapWithKeys(function ($item) {
                     return [$item['id'] => $item['name'].' 手机尾号: '.substr($item['phone'],-4)];
-            }))->default('');
+            }))->default(' ');
             //->ajax('/admin/api/client');
             $form->select('staff_id','导购员')->options(
                 Staff::all()->mapWithKeys(function ($item) {
